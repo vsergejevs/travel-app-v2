c4d706f84d70e0c1dca24cf855d33193bcb12847
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-07-17 05:27:25 +0100

    testing routing

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require('express');
 
 const app = express();
 
+// Routing
+// req and res are objects
+app.get('/', (req, res) => {
+  // res.status(200).send('Hello World'); // res is an object    send() is a method    status() is a method
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side', app: 'Natours' }); // another way to respond to get request by using json
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
