57865d84c5ecfcea92c1feae9d798c331f90892c
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-08-08 06:21:49 +0100

    added get request with URL parameter response

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json()); // middleware - modifys incoming request data. Here data from the body is added to the request object
 
-const localTours = JSON.parse(
+const offlineTours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
@@ -18,9 +18,9 @@ app.get('/api/v1/tours', (req, res) => {
   // (req, res) - route handler function
   res.status(200).json({
     status: 'success',
-    results: localTours.length,
+    results: offlineTours.length,
     data: {
-      tours: localTours, // in ES6 if a key and value has the same name, only one can be specified e.g. tours: tours
+      tours: offlineTours, // in ES6 if a key and value has the same name, only one can be specified e.g. tours: tours
     },
   });
 });
@@ -39,14 +39,14 @@ app.get('/api/v1/users', (req, res) => {
 app.post('/api/v1/tours', (req, res) => {
   console.log(req.body);
 
-  const newId = localTours[localTours.length - 1].id + 1; // generate new id as a next number of last tour object id
+  const newId = offlineTours[offlineTours.length - 1].id + 1; // generate new id as a next number of last tour object id
   const newTour = Object.assign({ id: newId }, req.body); // creates a new tour object
 
-  localTours.push(newTour); // push newly created tour to localTours array
+  offlineTours.push(newTour); // push newly created tour to offlineTours array
 
   fs.writeFile(
     `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(localTours),
+    JSON.stringify(offlineTours),
     (err) => {
       res.status(201).json({
         status: 'success',
@@ -58,6 +58,30 @@ app.post('/api/v1/tours', (req, res) => {
   );
 
   //res.send('Done'); //  always have to send something to finish request response cycle
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+
+  const id = req.params.id * 1; // convert string to number
+
+  // if the URL parameter exceeds the amount of tour objects in json or in database
+  if (id > offlineTours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  const tour = offlineTours.find((el) => el.id === id); // js find method to search inside array. Here searching for element with specified id in the url parameter
+
+  res.status(200).json({
+    status: 'success',
+    results: offlineTours.length,
+    data: {
+      tours: tour, // in ES6 if a key and value has the same name, only one can be specified
+    },
+  });
 });
 
 // app.get('/', (req, res) => {
