074511b20e67ecb612ed532abf9a12b71f18fb0f
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-08-23 08:21:39 +0100

    Tour routes moved to a seperate file and folder

---
+++ routes/tourRoutes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+
+const tourRouter = express.Router();
+
+tourRouter.route('/').get(getAllTours).post(createTour);
+tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
