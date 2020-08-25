dbb08ea463c01b6968ef1aef5fe23c3c24a3226f
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-08-25 04:39:48 +0100

    route handlers moved to seperate file/folder

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const fs = require('fs'); // filesystem
 const express = require('express');
 const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
@@ -23,179 +25,11 @@ app.use((req, res, next) => {
   next();
 });
 
-const offlineTours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
+// ROUTING now moved to seperate file/folder
 
-const localUsers = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/users.json`)
-);
-
-// ROUTING
-
-// HANDLER(controller) FUNCTIONS
-const getAllTours = (req, res) => {
-  // (req, res) - route handler function
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    results: offlineTours.length,
-    data: {
-      tours: offlineTours, // in ES6 if a key and value has the same name, only one can be specified e.g. tours: tours
-    },
-  });
-};
-
-const createTour = (req, res) => {
-  console.log(req.body);
-
-  const newId = offlineTours[offlineTours.length - 1].id + 1; // generate new id as a next number of last tour object id
-  const newTour = Object.assign({ id: newId }, req.body); // creates a new tour object
-
-  offlineTours.push(newTour); // push newly created tour to offlineTours array
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(offlineTours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-  //res.send('Done'); //  always have to send something to finish request response cycle
-};
-
-const getTour = (req, res) => {
-  console.log(req.params);
-
-  const id = req.params.id * 1; // convert string to number
-
-  // if the URL parameter exceeds the amount of tour objects in json or in database
-  if (id > offlineTours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
-  const tour = offlineTours.find((el) => el.id === id); // js find method to search inside array. Here searching for element with specified id in the url parameter
-
-  res.status(200).json({
-    status: 'success',
-    results: offlineTours.length,
-    data: {
-      tours: tour, // in ES6 if a key and value has the same name, only one can be specified
-    },
-  });
-};
-
-const updateTour = (req, res) => {
-  if (req.params.id * 1 > offlineTours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here...>',
-    },
-  });
-};
-
-const deleteTour = (req, res) => {
-  if (req.params.id * 1 > offlineTours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
-  res.status(204).json({
-    //  204 - no content
-    status: 'success',
-    data: null,
-  });
-};
-
-const getAllUsers = (req, res) => {
-  // (req, res) - route handler function
-  res.status(200).json({
-    status: 'success',
-    results: localUsers.length,
-    data: {
-      users: localUsers, // in ES6 if a key and value has the same name, only one can be specified
-    },
-  });
-};
-
-const createUser = (req, res) => {
-  // (req, res) - route handler function
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is note yet defined',
-  });
-};
-
-const getUser = (req, res) => {
-  // (req, res) - route handler function
-  // res.status(500).json({
-  //   status: 'error',
-  //   message: 'This route is note yet defined',
-  // });
-
-  console.log(req.params);
-
-  const id = req.params.id * 1; // convert string to number
-
-  // if the URL parameter exceeds the amount of tour objects in json or in database
-  if (id > localUsers.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
-  const user = localUsers.find((el) => el.id === id); // js find method to search inside array. Here searching for element with specified id in the url parameter
-
-  res.status(200).json({
-    status: 'success',
-    results: localUsers.length,
-    data: {
-      users: user, // in ES6 if a key and value has the same name, only one can be specified
-    },
-  });
-};
-
-const updateUser = (req, res) => {
-  // (req, res) - route handler function
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is note yet defined',
-  });
-};
-
-const deleteUser = (req, res) => {
-  // (req, res) - route handler function
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is note yet defined',
-  });
-};
+// HANDLER(controller) FUNCTIONS now moved to seperate file/folder
 
 // ROUTES
-// New version with chaining requests to route
-
-const userRouter = express.Router();
-
-userRouter.route('/').get(getAllUsers).post(createUser);
-userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
 app.use('/api/v1/tours', tourRouter); // Mounting a router (tourRouter) on a route /api/v1/tours
 app.use('/api/v1/users', userRouter);
