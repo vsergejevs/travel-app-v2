44c792edda48650b0c996f3c0cb62edc48c1a574
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-08-26 05:59:12 +0100

    removed duplicate code

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -86,13 +86,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > offlineTours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
   res.status(204).json({
     //  204 - no content
     status: 'success',
