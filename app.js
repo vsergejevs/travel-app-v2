0ef4c2e218ffa98f58842f3032c051762cfe4460
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-08-26 05:20:28 +0100

    added comments on what each middleware does

--- app.js
+++ app.js
@@ -7,18 +7,18 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // MIDDLEWARES
-app.use(morgan('dev'));
+app.use(morgan('dev')); // middleware to see in console.log information on what requests are made
 
 app.use(express.json()); // middleware - modifys incoming request data. Here data from the body is added to the request object
 
-// Middleware function
+// Middleware function to say hello - for fun
 app.use((req, res, next) => {
   // By adding next function, im telling express that this is going to be a middleware
   console.log('Hello from the middleware');
   next(); // Adding next() and calling it by using () at the end is crucial to complete req, res cycle
 });
 
-// Middleware function
+// Middleware function to console.log the time when a request is being made
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   console.log(req.requestTime);
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 
 // HANDLER(controller) FUNCTIONS now moved to seperate file/folder
 
-// ROUTES
+// ROUTES now moved to seperate file/folder below is route starting points
 
 app.use('/api/v1/tours', tourRouter); // Mounting a router (tourRouter) on a route /api/v1/tours
 app.use('/api/v1/users', userRouter);
@@ -60,9 +60,4 @@ app.use('/api/v1/users', userRouter);
 //   res.send('You can post to this endpoint/URL now');
 // });
 
-// SERVER
-
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
+module.exports = app;
