89abfe4afae1007b22faf1e0bab007deb9f340f0
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-08-27 19:58:18 +0100

    Added dotenv package to read environment variables

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 // SERVER
+const dotenv = require('dotenv'); // requiring dotenv package to read from config.env file
+dotenv.config({ path: './config.env' }); // letting node know where env variables are
+
 const app = require('./app');
+
+console.log(process.env);
 
 const port = 3000;
 app.listen(port, () => {
