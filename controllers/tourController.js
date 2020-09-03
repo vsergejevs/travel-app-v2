ff362cd14b841cd5d315aa776601a793d5619e8c
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-09-03 05:25:45 +0100

    tour field limiting implemented

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -56,6 +56,14 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    // 4) Field limiting to respond to api call with selected fields (like only tour name and )
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
     // EXECUTE QUERY
     const tours = await query;
 
