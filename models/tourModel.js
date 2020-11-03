04e8682e14dfbb17e728b1bfacc90702797ff0e2
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-11-03 04:31:47 +0000

    Added query middleware to measure time it takes to return matching documents that are not the secret tour

--- models/tourModel.js
+++ models/tourModel.js
@@ -104,6 +104,12 @@ tourSchema.pre(/^find/, function(next) {
   next();
 });
 
+tourSchema.post(/^find/, function(docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds`)
+  console.log(docs);
+  next();
+})
+
 // Mongoose data model
 const Tour = mongoose.model('Tour', tourSchema);
 
