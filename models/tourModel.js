daf8c277351cb6121eba396ce6c78031a12ee6f9
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-11-05 06:31:18 +0000

    Added tour name length validator

--- models/tourModel.js
+++ models/tourModel.js
@@ -2,12 +2,15 @@ const mongoose = require('mongoose');
 const slugify = require('slugify');
 
 // Mongoose schema
+// Fat model thin controller concept
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
     trim: true,
+    maxlength: [40,'A tour name must have less or equal than 40 characters'],
+    minlength: [10,'A tour name must have more or equal than 10 characters']
   },
   slug: String,
   duration: {
