fbaad5aa07982c3f5a26e84947d5a24d40010fc5
vsergejevs <valeriy.sergeyev7@gmail.com>
2020-12-04 04:38:47 +0000

    Small refactoring

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -8,11 +8,25 @@ const sendErrorDev = (err, res) => {
 }
 
 const sendErrorProd = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-}
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+  // Programming or other unknown error: don't leak error details
+  } else {
+    // 1) Log Error
+    console.error('ERROR 💥 ', err);
+    
+    // 2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!'
+    });
+  }
+};
 
 module.exports = (err, req, res, next) => {
   
