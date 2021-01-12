ae48d6ab1447b50557558d9a1a2472981d007ec1
vsergejevs <valeriy.sergeyev7@gmail.com>
2021-01-12 08:27:48 +0000

    Modify signup function to send also the custom role

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,7 +21,8 @@ exports.signup = catchAsync(async (req, res, next) => {
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
-    passwordChangedAt: req.body.passwordChangedAt
+    passwordChangedAt: req.body.passwordChangedAt,
+    role: req.body.role
   });
 
   const token = signToken(newUser._id);
@@ -97,3 +98,13 @@ exports.protect = catchAsync(async (req, res, next) => {
   req.user = currentUser;
   next();
 });
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    // roles ['admin', 'lead-guide']
+    if (!roles.includes(req.user.role)) {
+      return next (new AppError('You do not have permission to perform this action', 403))
+    };
+  }
+  next();
+};
