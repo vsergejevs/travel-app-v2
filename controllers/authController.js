b8dea66da37fd3b8d2fbceb12f6ea77224d743df
vsergejevs <valeriy.sergeyev7@gmail.com>
2021-01-17 17:40:52 +0000

    Add resetPassword middleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -143,9 +143,37 @@ try {
   await user.save({ validateBeforeSave: false });
 
   return next( new AppError('There was an error sending the email. Try again later!'), 500);
-}
-
-  
+} 
 });
 
-exports.resetPassword = (req, res, next) => {};
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  // 1) Get user based on the token
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+
+  console.log(hashedToken)
+
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() }
+  });
+
+  // 2) If token has not expired, and there is user, set the new password
+  if (!user) {
+    return next(new AppError('Token is invalid or has expired', 400));
+  }
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
+
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: 'success',
+    token
+  });
+
+});
