d57b8b6273e76aa6fc9b8c16c53a3e6232ee168c
vsergejevs <valeriy.sergeyev7@gmail.com>
2021-01-18 06:46:21 +0000

    Add createSendToken and updatePassword functions

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,6 +12,19 @@ const signToken = id => {
   });
 };
 
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+  
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user
+    }
+  });
+
+};
+
 // Middleware functions below
 
 exports.signup = catchAsync(async (req, res, next) => {
@@ -26,15 +39,8 @@ exports.signup = catchAsync(async (req, res, next) => {
     role: req.body.role
   });
 
-  const token = signToken(newUser._id);
+  createSendToken(newUser, 201, res)
 
-  res.status(201).json({
-    status: 'success',
-    token,
-    data: {
-      user: newUser
-    }
-  });
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -54,11 +60,7 @@ exports.login = catchAsync(async (req, res, next) => {
   }
 
   // 3) If everything OK send token back to client
-  const token = signToken(user._id);
-  res.status(200).json({
-    status: 'success',
-    token
-  });
+  createSendToken(user, 200, res)
 
 });
 
@@ -170,10 +172,31 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   user.passwordResetExpires = undefined;
   await user.save();
 
-  const token = signToken(user._id);
-  res.status(200).json({
-    status: 'success',
-    token
-  });
+  // 3) Update changedPasswordAt property for the user
+  // 4) Log the user in, send JWT
+  createSendToken(user, 200, res)
 
 });
+
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  // 1) Get user from collection
+  const user = await User.findById(req.user.id).select('+password');
+
+  // 2) Check if  POSTed current password is correct
+  if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+    return next(new AppError('Your current password is wrong', 401));
+  }
+
+  // 3) If so, update password
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+
+  // 4) Log user in, send JWT
+  createSendToken(user, 200, res);
+
+  // augšā uztaisīja funkciju createSendToken()
+  // userRoutes.js uztaisīja jaunu router
+  // Postman uztaisīja jaunu request - PATCH updateMyPassword ar dažādām parolēm json body un authorization jā'bem bearer token
+
+})
