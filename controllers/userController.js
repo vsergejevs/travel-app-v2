a98a44b3f91c8b1826c19b36bb9fd86ec5cbe7b6
vsergejevs <valeriy.sergeyev7@gmail.com>
2021-01-19 07:17:31 +0000

    Add middleware to update user details

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const User = require('./../models/userModel'); // importing the tourModel.js
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
 
 // HANDLER(controller) FUNCTIONS
@@ -14,6 +15,29 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     results: users.length,
     data: {
       users, // in ES6 if a key and value has the same name, only one can be specified e.g. tours: tours
+    }
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError('This route is not for password updates. Please use /updateMyPassword.', 400)
+    );
+  }
+  // 2) Filtered out unwanted field names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // 3) Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true
+  });
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser
     }
   });
 });
