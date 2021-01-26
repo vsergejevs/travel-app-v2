e4b392da0b8eb5401fae3a001b41bd63bedfdbb8
vsergejevs <valeriy.sergeyev7@gmail.com>
2021-01-26 13:01:46 +0000

    Create route for user reviews

---
+++ routes/reviewRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const reviewController = require('./../controllers/reviewController');
+const authController = require('./../controllers/authController');
+
+const router = express.Router({ mergeParams: true });
+
+router.use(authController.protect);
+
+router
+  .route('/')
+  .get()
+  .post();
+
+  
