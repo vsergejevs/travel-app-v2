4eada6465a878dd8b2089a868e3683584ff6af2a
vsergejevs <valeriy.sergeyev7@gmail.com>
2021-01-27 06:38:42 +0000

    Add controllers to all review routes

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -8,19 +8,23 @@ router.use(authController.protect);
 
 router
   .route('/')
-  .get()
-  .post();
+  .get(reviewController.getAllReviews)
+  .post(
+    authController.restrictTo('user'),
+    reviewController.setTourUserIds,
+    reviewController.createReview
+  );
 
-  router
-    .route('/:id')
-    .get(reviewController.getReview)
-    .patch(
-      authController.restrictTo('user', 'admin'),
-      reviewController.updateReview
-    )
-    .delete(
-      authController.restrictTo('user', 'admin'),
-      reviewController.deleteReview
-    );
+router
+  .route('/:id')
+  .get(reviewController.getReview)
+  .patch(
+    authController.restrictTo('user', 'admin'),
+    reviewController.updateReview
+  )
+  .delete(
+    authController.restrictTo('user', 'admin'),
+    reviewController.deleteReview
+  );
 
 module.exports = router;
