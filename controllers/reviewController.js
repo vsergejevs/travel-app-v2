44763de0c5170de10d18b4e61c49075085daa250
vsergejevs <valeriy.sergeyev7@gmail.com>
2021-01-28 20:08:28 +0000

    Add set tour user ids

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,6 +1,14 @@
 const Review = require('./../models/reviewModel');
 const catchAsync = require('./../utils/catchAsync');
 
+
+exports.setTourUserIds = (req, res, next) => {
+  // Allow nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+  next();
+};
+
 exports.getAllReviews = catchAsync(async (req, res, next) => {
   const reviews = await Review.find();
 
