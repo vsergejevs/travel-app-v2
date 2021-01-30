90c34fbb775c937190b5cd2e98ad488d4e6d1ff8
vsergejevs <valeriy.sergeyev7@gmail.com>
2021-01-30 18:56:38 +0000

    Temporal code refactor

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,29 +2,28 @@ const express = require('express');
 const reviewController = require('./../controllers/reviewController');
 const authController = require('./../controllers/authController');
 
-const router = express.Router({ mergeParams: true });
+const router = express.Router();
 
-router.use(authController.protect);
 
 router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
+    authController.protect,
     authController.restrictTo('user'),
-    reviewController.setTourUserIds,
     reviewController.createReview
   );
 
-router
-  .route('/:id')
-  .get(reviewController.getReview)
-  .patch(
-    authController.restrictTo('user', 'admin'),
-    reviewController.updateReview
-  )
-  .delete(
-    authController.restrictTo('user', 'admin'),
-    reviewController.deleteReview
-  );
+// router
+//   .route('/:id')
+//   .get(reviewController.getReview)
+//   .patch(
+//     authController.restrictTo('user', 'admin'),
+//     //reviewController.updateReview
+//   )
+//   .delete(
+//     authController.restrictTo('user', 'admin'),
+//     //reviewController.deleteReview
+//   );
 
 module.exports = router;
