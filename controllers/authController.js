5e60f93218869c1988e428bb706373fb71c41c83
vsergejevs <valeriy.sergeyev7@gmail.com>
2021-01-30 12:05:46 +0000

    Fix bug of not being able to create user reviews

--- controllers/authController.js
+++ controllers/authController.js
@@ -119,10 +119,10 @@ exports.restrictTo = (...roles) => {
   return (req, res, next) => {
     // roles ['admin', 'lead-guide']
     if (!roles.includes(req.user.role)) {
-      return next (new AppError('You do not have permission to perform this action', 403))
-    };
-  }
-  next();
+      return next (new AppError('You do not have permission to perform this action', 403));
+    }
+    next();
+  };
 };
 
 exports.forgotPassword = catchAsync(async (req, res, next) => {
