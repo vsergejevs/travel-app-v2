76d0606fb8c897af85b5459c709106710f8ee72a
vsergejevs <valeriy.sergeyev7@gmail.com>
2021-02-03 04:58:50 +0000

    Direct tourId/reviews to reviewRouter, remove review creation router

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,10 +2,13 @@ const express = require('express');
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
 const reviewController = require('./../controllers/reviewController');
+const reviewRouter = require('./../routes/reviewRoutes');
 
 const router = express.Router();
 
 // router.param('id', tourController.checkID); // param method searches for the given(id) parameter
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router
   .route('/top-5-cheap')
@@ -28,14 +31,6 @@ router
     authController.protect, 
     authController.restrictTo('admin', 'lead-guide'), 
     tourController.deleteTour);
-
-  router
-  .route('/:tourId/reviews')
-  .post(
-    authController.protect, 
-    authController.restrictTo('user'), 
-    reviewController.createReview
-  );
   
 
 module.exports = router;
