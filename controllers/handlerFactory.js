966753a63d293116d3605682baf89acf16c3dc2c
vsergejevs <valeriy.sergeyev7@gmail.com>
2021-02-05 05:50:02 +0000

    Add handler to update one document

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -14,3 +14,22 @@ exports.deleteOne = Model => catchAsync(async (req, res, next) => {
     data: null,
   });
 });
+
+
+exports.updateOne = Model => catchAsync(async (req, res, next) => {
+  const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+    new: true, //updated doc will be returned
+    runValidators: true,
+  });
+
+  if(!doc) {
+    return next(new AppError('No document found with that ID', 404))
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      data: doc
+    },
+  });
+});
