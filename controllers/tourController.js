6ae03140c0cb249322c75f82ddda58444fd3e8e6
vsergejevs <valeriy.sergeyev7@gmail.com>
2021-02-08 23:31:56 +0000

    Add factory function to get one tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -34,75 +34,10 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = factory.getOne(Tour);
-
-// exports.getTour = catchAsync(async (req, res, next) => {
-//   const tour = await Tour.findById(req.params.id).populate('reviews');
-
-//   if(!tour) {
-//     return next(new AppError('No tour found with that ID', 404))
-//   }
-
-//   res.status(200).json({
-//     status: 'success',
-//     data: {
-//       tour, // in ES6 if a key and value has the same name, only one can be specified e.g. tours: tours
-//     },
-//   });
-
-//   console.log('URL arameter here is:');
-//   console.log(req.params);
-// });
-
+exports.getTour = factory.getOne(Tour, { path: 'reviews' });
 exports.createTour = factory.createOne(Tour);
-
-// exports.createTour = catchAsync(async (req, res, next) => {
-//   const newTour = await Tour.create(req.body);
-//   // Tour.create returns a promise that I'm awaiting and I can store the newly created document in newTour variable
-//   // then send it with response to the client below in - tour: newTour
-//   // if an error occurs - it is being sent back from the catchAsync error catch
-
-//   res.status(201).json({
-//     status: 'success',
-//     data: {
-//       tour: newTour,
-//     }
-//   });
-// });
-
 exports.updateTour = factory.updateOne(Tour);
-
-// exports.updateTour = catchAsync(async (req, res, next) => {
-//   const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
-//     new: true, //updated document will be returned
-//     runValidators: true,
-//   });
-
-//   if(!tour) {
-//     return next(new AppError('No tour found with that ID', 404))
-//   }
-
-//   res.status(200).json({
-//     status: 'success',
-//     data: {
-//       tour,
-//     },
-//   });
-// });
-
 exports.deleteTour = factory.deleteOne(Tour);
-// exports.deleteTour = catchAsync(async (req, res, next) => {
-//   const tour = await Tour.findByIdAndDelete(req.params.id);
-
-//   if (!tour) {
-//     return next(new AppError('No tour found with that ID', 404))
-//   }
-
-//   res.status(204).json({
-//     status: 'success',
-//     data: null,
-//   });
-// });
 
 // MongoDB aggregation pipelines
 
