35a895378b9c8ce6a5599fe9765d4f429b3e2965
vsergejevs <valeriy.sergeyev7@gmail.com>
2021-02-08 23:32:20 +0000

    Add factory function to get one user

--- controllers/userController.js
+++ controllers/userController.js
@@ -55,39 +55,11 @@ exports.createUser = (req, res) => {
   // (req, res) - route handler function
   res.status(500).json({
     status: 'error',
-    message: 'This route is note yet defined',
+    message: 'This route is not defined! Please use /signup instead.',
   });
 };
 
-exports.getUser = (req, res) => {
-  // (req, res) - route handler function
-  // res.status(500).json({
-  //   status: 'error',
-  //   message: 'This route is note yet defined',
-  // });
-
-  console.log(req.params);
-
-  const id = req.params.id * 1; // convert string to number
-
-  // if the URL parameter exceeds the amount of tour objects in json or in database
-  if (id > localUsers.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
-  const user = localUsers.find((el) => el.id === id); // js find method to search inside array. Here searching for element with specified id in the url parameter
-
-  res.status(200).json({
-    status: 'success',
-    results: localUsers.length,
-    data: {
-      users: user, // in ES6 if a key and value has the same name, only one can be specified
-    },
-  });
-};
+exports.getUser = factory.getOne(User);
 
 exports.deleteMe = catchAsync(async (req, res, next) => {
   await User.findByIdAndUpdate(req.user.id, { active: false });
