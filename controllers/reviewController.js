d3a525d9b02b0c846a789302eccd52cd43ede552
vsergejevs <valeriy.sergeyev7@gmail.com>
2021-02-08 23:02:16 +0000

    Add factory function to create review

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -25,16 +25,6 @@ exports.setTourUserIds = (req, res, next) => {
   next();
 };
 
-exports.createReview = catchAsync(async (req, res, next) => {
-  const newReview = await Review.create(req.body);
-
-  res.status(201).json({
-    status: 'success',
-    data: {
-      review: newReview
-    }
-  });
-});
-
+exports.createReview = factory.createOne(Review);
 exports.deleteReview = factory.deleteOne(Review);
 exports.updateReview = factory.updateOne(Review);
