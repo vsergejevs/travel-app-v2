bdc226cccde160fc9a1e9025a256a9f1e552d368
vsergejevs <valeriy.sergeyev7@gmail.com>
2021-02-08 23:31:25 +0000

    Add factory function to get one review

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -24,7 +24,7 @@ exports.setTourUserIds = (req, res, next) => {
   if(!req.body.user) req.body.user = req.user.id;
   next();
 };
-
+exports.getReview = factory.getOne(Review);
 exports.createReview = factory.createOne(Review);
 exports.deleteReview = factory.deleteOne(Review);
 exports.updateReview = factory.updateOne(Review);
