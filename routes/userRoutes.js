79a43f5aaf75b7c03d122c607ec4148c11447f1d
vsergejevs <valeriy.sergeyev7@gmail.com>
2021-02-14 17:31:23 +0000

    Add user routes protection

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,16 +3,20 @@ const userController = require('./../controllers/userController');
 const authController = require('./../controllers/authController');
 const router = express.Router();
 
-router.get('/me', authController.protect, userController.getMe, userController.getUser);
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
-router.patch('/updateMyPassword', authController.protect, authController.updatePassword);
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+// Protect all routes after this middleware
+router.use(authController.protect);
+
+router.get('/me', userController.getMe, userController.getUser);
+router.patch('/updateMyPassword', authController.updatePassword);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
+
+router.use(authController.restrictTo('admin'));
 
 router
   .route('/')
