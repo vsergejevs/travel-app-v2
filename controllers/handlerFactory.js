490c551440becb5c1a48626f6859aebb3ee4f42e
vsergejevs <valeriy.sergeyev7@gmail.com>
2021-02-17 19:43:13 +0000

    Add comment with .explain() method

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -85,6 +85,7 @@ exports.getAll = Model => catchAsync(async (req, res, next) => {
     .limitFields()
     .paginate();
   const doc = await features.query;
+  // const doc = await features.query.explain();
 
   // SEND RESPONSE
   res.status(200).json({
