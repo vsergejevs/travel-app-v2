89b8276f4758c770a006465a99a7cb4d100270f1
vsergejevs <valeriy.sergeyev7@gmail.com>
2021-02-28 16:20:13 +0000

    Add functionality to search tours from startlocation on map

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -96,3 +96,32 @@ exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
     }
   });
 });
+
+// /tours-within/:distance/center/:latlng/unit/:unit
+// /tours-within/233/center/-40,45/unit/mi
+exports.getToursWithin = catchAsync(async (req, res, next) => {
+  const { distance, latlng, unit } = req.params; // using object destructuring here https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment#object_destructuring 
+  const [lat, lng] = latlng.split(',');
+
+  // 
+  const radius = unit === 'mi'? distance / 3963.2 : distance / 6378.1;
+
+  if(!lat || !lng) {
+    next(new AppError('Please provide latitude and longitude in the format lat,lng', 400))
+  }
+  // Check data
+  console.log(distance, lat, lng, unit)
+
+  // Find documents within a certain distance of a starting point
+  const tours = await Tour.find({ 
+    startLocation: { $geoWithin: { $centerSphere: [[lng, lat], radius] } } 
+  });
+
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      data:tours
+    }
+  });
+});
