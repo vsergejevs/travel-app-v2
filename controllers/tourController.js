af188431da26e486f6d5f74372544eeb96d9695e
vsergejevs <valeriy.sergeyev7@gmail.com>
2021-03-02 06:33:47 +0000

    Add functionality to get distances to tours from a given point

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -107,7 +107,7 @@ exports.getToursWithin = catchAsync(async (req, res, next) => {
   const radius = unit === 'mi'? distance / 3963.2 : distance / 6378.1;
 
   if(!lat || !lng) {
-    next(new AppError('Please provide latitude and longitude in the format lat,lng', 400))
+    next(new AppError('Please provide latitude and longitude in the format lat,lng', 400));
   }
   // Check data
   console.log(distance, lat, lng, unit)
@@ -124,4 +124,42 @@ exports.getToursWithin = catchAsync(async (req, res, next) => {
       data:tours
     }
   });
+});
+
+exports.getDistances = catchAsync(async (req, res, next) => {
+  const { distance, latlng, unit } = req.params; // using object destructuring here https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment#object_destructuring 
+  const [lat, lng] = latlng.split(',');
+
+  const multiplier = unit === 'mi' ? 0.000621371 : 0.001;
+
+  if(!lat || !lng) {
+    next(new AppError('Please provide latitude and longitude in the format lat,lng', 400));
+  }
+
+const distances = await Tour.aggregate([
+  {
+    $geoNear: {
+      near: {
+        type: 'Point',
+        coordinates: [lng * 1, lat * 1]
+      },
+      distanceField: 'distance',
+      distanceMultiplier: multiplier
+    }
+  },
+  {
+    $project: {
+      distance: 1,
+      name: 1
+    }
+  }
+]);
+
+res.status(200).json({
+  status: 'success',
+  data: {
+    data: distances
+  }
+});
+
 });
