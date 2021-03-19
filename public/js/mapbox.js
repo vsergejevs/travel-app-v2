f0a79ac986711419199e7cff2948529e744ee0c0
vsergejevs <valeriy.sergeyev7@gmail.com>
2021-03-19 05:50:04 +0000

    Initial commit

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,3 +1,8 @@
-console.log('hello from the client side 💯');
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
-console.log(locations);
+
+mapboxgl.accessToken = 'pk.eyJ1IjoidnNlcmdleWV2IiwiYSI6ImNrbWY5ZHVkMDB1aHUycnA1b2hyMmhoYWEifQ.kDNxctFydEwGgwnWkWfLyg';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11'
+});
