1eb3c5a1b094ac219263dcb1831b3aeb2f47586e
vsergejevs <valeriy.sergeyev7@gmail.com>
2021-03-19 06:10:42 +0000

    Center map to set coordinates

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -4,5 +4,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoidnNlcmdleWV2IiwiYSI6ImNrbWY5ZHVkMDB1aHUycnA1b
 
 var map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/vsergeyev/ckmfw7xsyo6bz17l90vh2tjpt'
+  style: 'mapbox://styles/vsergeyev/ckmfw7xsyo6bz17l90vh2tjpt',
+  center: [-122.29286, 38.294065],
+  zoom: 5
 });
