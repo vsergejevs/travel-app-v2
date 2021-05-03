88ad0a7ea2e0e9b7a5aa82e56b7dd972273e873f
vsergejevs <valeriy.sergeyev7@gmail.com>
2021-05-03 17:26:41 +0100

    Add tour locations to mapbox map

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,10 +1,31 @@
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
+console.log(locations + ' This log lives in mapbox.js');
 
 mapboxgl.accessToken = 'pk.eyJ1IjoidnNlcmdleWV2IiwiYSI6ImNrbWY5ZHVkMDB1aHUycnA1b2hyMmhoYWEifQ.kDNxctFydEwGgwnWkWfLyg';
 
 var map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/vsergeyev/ckmfw7xsyo6bz17l90vh2tjpt',
-  center: [-122.29286, 38.294065],
-  zoom: 5
+  // center: [-122.29286, 38.294065],
+  // zoom: 5
 });
+
+// figure out position of the map based on tour location points, bounds will be the area displayed on the map
+const bounds = new mapboxgl.LatLngBounds();
+
+locations.forEach(loc => {
+  // Create marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+  
+  // Add marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom'
+  }).setLngLat(loc.coordinates).addTo(map);
+
+  // Extend map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds)
