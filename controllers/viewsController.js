838ee96142e254f5ca7a6a278a430a0697f08416
vsergejevs <valeriy.sergeyev7@gmail.com>
2021-05-13 08:46:50 +0100

    Create login form views controller

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,27 +1,32 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 
-exports.getOverview = catchAsync( async (req, res, next) => {
+exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) Get tour data from collection
   const tours = await Tour.find();
 
   // 2) Build template
-  // 3) Render that template using tour data from 1) 
+  // 3) Render that template using tour data from 1)
   res.status(200).render('overview', {
     title: 'All Tours',
-    tours: tours
+    tours: tours,
   });
 });
 
-exports.getTour = catchAsync( async (req, res) => {
-  const tour = await Tour.findOne({slug: req.params.slug}).populate({
+exports.getTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
-    fields: 'review rating user'
+    fields: 'review rating user',
   });
 
   res.status(200).render('tour', {
     title: `${tour.name} Tour`,
-    tour
+    tour,
   });
 });
 
+exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', {
+    title: 'Log into your account',
+  });
+};
