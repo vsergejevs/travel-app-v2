69b85bf908a94b54e4405ffdea9d21f891511308
vsergejevs <valeriy.sergeyev7@gmail.com>
2021-06-06 07:22:21 +0100

    Create a js starting point file

---
+++ public/js/index.js
@@ -0,0 +1,25 @@
+import '@babel/polyfill';
+import { displayMap } from './mapbox';
+import { login } from './login';
+
+// index.js is for getting data from the user and to delegate it to other modules/functions
+
+// DOM ELEMENTS
+const mapBox = document.getElementById('map');
+const loginForm = document.querySelector('.form');
+
+// DELEGATION
+if (mapBox) {
+  const locations = JSON.parse(mapBox.dataset.locations);
+  console.log(locations + ' This log lives in mapbox.js');
+  displayMap(locations);
+}
+
+if (loginForm) {
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault(); // prevents form from loading other pages
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
+  });
+}
