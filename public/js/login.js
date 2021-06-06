c1a932fc578e404a37eeaf51360864c9f4f19c51
vsergejevs <valeriy.sergeyev7@gmail.com>
2021-06-06 07:54:50 +0100

    Add custom alerts

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
-
 import axios from 'axios';
+import { showAlert } from './alerts';
 
 export const login = async (email, password) => {
   // console.log(email, password);
@@ -15,14 +15,14 @@ export const login = async (email, password) => {
     });
 
     if (res.data.status === 'success') {
-      alert('Logged in successfully');
+      showAlert('success', 'Logged in successfully');
       window.setTimeout(() => {
         location.assign('/');
       }, 1500);
     }
     console.log(res, ' this lives in public/js/login.js');
   } catch (err) {
-    alert(err.response.data.message);
+    showAlert('error', err.response.data.message);
   }
 };
 //test
