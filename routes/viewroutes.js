46f4957f500608888004bc4438f49a5e2cfc8b6d
vsergejevs <valeriy.sergeyev7@gmail.com>
2021-06-23 04:31:38 +0100

    Add route to change user data using form

--- routes/viewroutes.js
+++ routes/viewroutes.js
@@ -9,4 +9,10 @@ router.get('/tour/:slug', authController.isLoggedin, viewsController.getTour);
 router.get('/login', authController.isLoggedin, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
 
+router.post(
+  '/submit-user-data',
+  authController.protect,
+  viewsController.updateUserData
+);
+
 module.exports = router;
