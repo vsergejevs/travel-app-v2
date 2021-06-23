db0c8ef35c4b8211c9c9cebc9063e013550ad25b
vsergejevs <valeriy.sergeyev7@gmail.com>
2021-06-23 04:29:34 +0100

    Add functionality to choose user data form based on different classes

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,15 @@
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login, logout } from './login';
+import { updateData } from './updateSettings';
 
 // index.js is for getting data from the user and to delegate it to other modules/functions
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
-const loginForm = document.querySelector('.form');
+const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
 
 // DELEGATION
 if (mapBox) {
@@ -26,3 +28,11 @@ if (loginForm) {
 }
 
 if (logOutBtn) logOutBtn.addEventListener('click', logout);
+
+if (userDataForm)
+  userDataForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    updateData(name, email);
+  });
