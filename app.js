8d17c5135f597dfcfa1d504284962ccadc438ee5
vsergejevs <valeriy.sergeyev7@gmail.com>
2021-06-24 05:34:17 +0100

    Remove console.log

--- app.js
+++ app.js
@@ -114,7 +114,7 @@ app.use(
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   console.log(req.requestTime + ' This log lives in app.js');
-  console.log(req.cookies);
+  // console.log(req.cookies);
   next();
 });
 
