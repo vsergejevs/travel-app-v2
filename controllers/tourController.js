45be071791e2ac9949dc1eb29769f74cf3638222
vsergejevs <valeriy.sergeyev7@gmail.com>
2021-07-04 08:31:48 +0100

    Add tour image uploading controllers

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,9 +1,37 @@
+const multer = require('multer');
+const sharp = require('sharp');
 const Tour = require('./../models/tourModel'); // importing the tourModel.js
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('../utils/appError');
 const factory = require('./handlerFactory');
 
 // HANDLER(controller) FUNCTIONS
+
+const multerStorage = multer.memoryStorage();
+
+//Checks if uploaded file is an image
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image! Please upload only images.', 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploadTourImages = upload.fields([
+  { name: 'imageCover', maxCount: 1 },
+  { name: 'images', maxCount: 3 }
+]);
+
+exports.resizeTourImages = (req, res, next) => {
+  console.log(req.files);
+  next();
+}
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -23,7 +51,7 @@ exports.deleteTour = factory.deleteOne(Tour);
 exports.getTourStats = catchAsync(async (req, res, next) => {
   const stats = await Tour.aggregate([
     {
-      $match: { ratingsAverage: { $gte: 4.5 } }
+      $match: { ratingsAverage: { $gte: 4.5 } },
     },
     {
       $group: {
@@ -33,12 +61,12 @@ exports.getTourStats = catchAsync(async (req, res, next) => {
         avgRating: { $avg: '$ratingsAverage' },
         avgPrice: { $avg: '$price' },
         minPrice: { $min: '$price' },
-        maxPrice: { $max: '$price' }
-      }
+        maxPrice: { $max: '$price' },
+      },
     },
     {
-      $sort: { avgPrice: 1 }
-    }
+      $sort: { avgPrice: 1 },
+    },
     // {
     //   $match: { _id: { $ne: 'EASY' } }
     // }
@@ -56,111 +84,120 @@ exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
   const year = req.params.year * 1; // 2021
   const plan = await Tour.aggregate([
     {
-      $unwind: '$startDates'
+      $unwind: '$startDates',
     },
     {
       $match: {
         startDates: {
           $gte: new Date(`${year}-01-01`),
-          $lte: new Date(`${year}-12-31`)
-        }
-      }
+          $lte: new Date(`${year}-12-31`),
+        },
+      },
     },
     {
       $group: {
         _id: { $month: '$startDates' },
         numTourStarts: { $sum: 1 },
-        tours: { $push: '$name' }
-      }
+        tours: { $push: '$name' },
+      },
     },
     {
-      $addFields: { month: '$_id' }
+      $addFields: { month: '$_id' },
     },
     {
       $project: {
-        _id: 0
-      }
+        _id: 0,
+      },
     },
     {
-      $sort: { numTourStarts: -1 }
+      $sort: { numTourStarts: -1 },
     },
     {
-      $limit: 12
-    }
+      $limit: 12,
+    },
   ]);
 
   res.status(200).json({
     status: 'success',
     data: {
-      plan
-    }
+      plan,
+    },
   });
 });
 
 // /tours-within/:distance/center/:latlng/unit/:unit
 // /tours-within/233/center/-40,45/unit/mi
 exports.getToursWithin = catchAsync(async (req, res, next) => {
-  const { distance, latlng, unit } = req.params; // using object destructuring here https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment#object_destructuring 
+  const { distance, latlng, unit } = req.params; // using object destructuring here https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment#object_destructuring
   const [lat, lng] = latlng.split(',');
 
-  // 
-  const radius = unit === 'mi'? distance / 3963.2 : distance / 6378.1;
+  //
+  const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
 
-  if(!lat || !lng) {
-    next(new AppError('Please provide latitude and longitude in the format lat,lng', 400));
+  if (!lat || !lng) {
+    next(
+      new AppError(
+        'Please provide latitude and longitude in the format lat,lng',
+        400
+      )
+    );
   }
   // Check data
-  console.log(distance, lat, lng, unit)
+  console.log(distance, lat, lng, unit);
 
   // Find documents within a certain distance of a starting point
-  const tours = await Tour.find({ 
-    startLocation: { $geoWithin: { $centerSphere: [[lng, lat], radius] } } 
+  const tours = await Tour.find({
+    startLocation: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
   });
 
   res.status(200).json({
     status: 'success',
     results: tours.length,
     data: {
-      data:tours
-    }
+      data: tours,
+    },
   });
 });
 
 // Get distances to tours from point
 exports.getDistances = catchAsync(async (req, res, next) => {
-  const { distance, latlng, unit } = req.params; // using object destructuring here https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment#object_destructuring 
+  const { distance, latlng, unit } = req.params; // using object destructuring here https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment#object_destructuring
   const [lat, lng] = latlng.split(',');
 
   const multiplier = unit === 'mi' ? 0.000621371 : 0.001;
 
-  if(!lat || !lng) {
-    next(new AppError('Please provide latitude and longitude in the format lat,lng', 400));
+  if (!lat || !lng) {
+    next(
+      new AppError(
+        'Please provide latitude and longitude in the format lat,lng',
+        400
+      )
+    );
   }
 
-const distances = await Tour.aggregate([
-  {
-    $geoNear: {
-      near: {
-        type: 'Point',
-        coordinates: [lng * 1, lat * 1]
+  const distances = await Tour.aggregate([
+    {
+      $geoNear: {
+        near: {
+          type: 'Point',
+          coordinates: [lng * 1, lat * 1],
+        },
+        distanceField: 'distance',
+        distanceMultiplier: multiplier,
       },
-      distanceField: 'distance',
-      distanceMultiplier: multiplier
-    }
-  },
-  {
-    $project: {
-      distance: 1,
-      name: 1
-    }
-  }
-]);
+    },
+    {
+      $project: {
+        distance: 1,
+        name: 1,
+      },
+    },
+  ]);
 
-res.status(200).json({
-  status: 'success',
-  data: {
-    data: distances
-  }
-});
-
+  res.status(200).json({
+    status: 'success',
+    data: {
+      data: distances,
+    },
+  });
 });
