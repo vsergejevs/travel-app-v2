b97769b906fdf338cc96744ea686b1ef0601467a
vsergejevs <valeriy.sergeyev7@gmail.com>
2021-07-08 16:07:41 +0100

    Mar function as async]

--- controllers/userController.js
+++ controllers/userController.js
@@ -33,19 +33,19 @@ const upload = multer({
 
 exports.uploadUserPhoto = upload.single('photo');
 
-exports.resizeUserPhoto = (req, res, next) => {
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
   if (!req.file) return next();
 
   req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
 
-  sharp(req.file.buffer)
+  await sharp(req.file.buffer)
     .resize(500, 500)
     .toFormat('jpeg')
     .jpeg({ quality: 90 })
     .toFile(`public/img/users/${req.file.filename}`);
 
   next();
-};
+});
 
 // Function to filter out which fields can be updated only
 const filterObj = (obj, ...allowedFields) => {
