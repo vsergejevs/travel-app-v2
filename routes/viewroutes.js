ea39de2b9df9db89216951884b11f562aa115f26
vsergejevs <valeriy.sergeyev7@gmail.com>
2021-08-11 22:42:55 +0100

    Create checkout controller and add to route

--- routes/viewroutes.js
+++ routes/viewroutes.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
+const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
-router.get('/', authController.isLoggedin, viewsController.getOverview);
+router.get(
+  '/',
+  bookingController.createBookingCheckout,
+  authController.isLoggedin,
+  viewsController.getOverview
+);
 router.get('/tour/:slug', authController.isLoggedin, viewsController.getTour);
 router.get('/login', authController.isLoggedin, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
